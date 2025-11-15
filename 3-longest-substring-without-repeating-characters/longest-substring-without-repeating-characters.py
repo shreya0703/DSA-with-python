7e658a2e9b0bcfe5fa8316ea88cd4d501be47b03
shreya0703 <147245183+shreya0703@users.noreply.github.com>
2025-11-15 22:48:39 +0530

    Time: 16 ms (66.11%) | Memory: 17.8 MB (52.56%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,19 +1,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # hash array of size 256 initialized with -1
-        hash_map = [-1] * 256
-        l = 0 
-        r = 0
-        n = len(s)
+        last_seen = {}
+        l = 0
         max_len = 0
 
-        while r<n:
-            char_index = ord(s[r])    # convert char → ASCII index ,ord() is a built-in Python function that returns the ASCII (or Unicode) integer code of a character.
-            if hash_map[char_index] != -1:
-                if hash_map[char_index] >= l:# if the duplicate index is inside the current window
-                     l = hash_map[char_index] + 1 # move left pointer
-            hash_map[char_index] = r
-            max_len = max(max_len, r - l + 1)
-            r+=1
-        return max_len
+        for r, ch in enumerate(s):
+            if ch in last_seen and last_seen[ch] >= l:
+                l = last_seen[ch] + 1
 
+            last_seen[ch] = r
+            max_len = max(max_len, r - l + 1)
+
+        return max_len
+        
