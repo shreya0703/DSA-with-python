366d32dae922d8767ef2ced59776a8f1c365c103
shreya0703 <147245183+shreya0703@users.noreply.github.com>
2025-11-22 13:42:47 +0530

    Time: 31 ms (60.83%) | Memory: 18.7 MB (62.14%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,18 +1,22 @@
 class Solution:
-    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        prefix = 0
+    def atMost(self, nums, goal):
+        if goal < 0:
+            return 0
+        
+        l = 0
+        sum_ = 0
         count = 0
-        freq = {0: 0}
-        freq[0] = 1   # one way to have prefix sum 0 before starting
-
-        for num in nums:
-            prefix += num
-
-            # we need prefix - goal seen before
-            if prefix - goal in freq:
-                count += freq[prefix - goal]
-
-            # store current prefix
-            freq[prefix] = freq.get(prefix, 0) + 1
-
+        
+        for r in range(len(nums)):
+            sum_ += nums[r]
+            
+            while sum_ > goal:
+                sum_ -= nums[l]
+                l += 1
+            
+            count += (r - l + 1)
+        
         return count
+    
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        return self.atMost(nums, goal) - self.atMost(nums, goal - 1)
