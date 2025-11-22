75529f2b7527b602034748ce9eaca8f4e8ba1d61
shreya0703 <147245183+shreya0703@users.noreply.github.com>
2025-11-22 13:45:56 +0530

    Time: 30 ms (64.62%) | Memory: 18.7 MB (79.15%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,22 +1,23 @@
 class Solution:
-    def atMost(self, nums, goal):
-        if goal < 0:
-            return 0
-        
-        l = 0
-        sum_ = 0
-        count = 0
-        
-        for r in range(len(nums)):
-            sum_ += nums[r]
-            
-            while sum_ > goal:
-                sum_ -= nums[l]
-                l += 1
-            
-            count += (r - l + 1)
-        
-        return count
-    
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        return self.atMost(nums, goal) - self.atMost(nums, goal - 1)
+        
+        def atMost(k):
+            if k < 0:
+                return 0
+            
+            l = 0
+            sum_ = 0
+            count = 0
+            
+            for r in range(len(nums)):
+                sum_ += nums[r]
+                
+                while sum_ > k:
+                    sum_ -= nums[l]
+                    l += 1
+                
+                count += (r - l + 1)
+            
+            return count
+        
+        return atMost(goal) - atMost(goal - 1)
